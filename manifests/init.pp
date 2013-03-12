2d25846c80b396e9a9ce877e5c0ada108bd7ace1
Jeff Li <jafelee@gmail.com>
2013-03-12 16:19:07 +0800

    fix the grammar error

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 
 class vagrant {
   package { 'Vagrant':
-    source   => 'http://127.0.0.1:12345/Vagrant.dmg'
+    source   => 'http://127.0.0.1:12345/Vagrant.dmg',
     provider => pkgdmg,
   }
 }
